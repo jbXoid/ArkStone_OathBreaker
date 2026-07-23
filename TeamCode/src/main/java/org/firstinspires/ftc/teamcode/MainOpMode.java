474f12d6dafb0fad746cfbfba2503ea3657fddb9
jbxoid <romanoidohm@gmail.com>
2026-07-24 00:14:39 +0300

    Created opMode and figured out how to set up http proxy in Android Studio

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class MainOpMode extends LinearOpMode {
+
+
+    public void runOpMode() {
+
+    }
+
+}
